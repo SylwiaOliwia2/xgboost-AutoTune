794c72a077f312ff30f307a167ec5a380eb59e44
Sylwia Mielnicka <sylwia.kucharska11@gmail.com>
2018-01-22 00:20:16 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='xgboost_autotune',
+    version='0.1',
+    description='Xgboost_autotune is a Python module which auto-tunes bosting models.',
+    author='Sylwia Mielnicka',
+    author_email='hello@sylwiamielnicka.com',
+    url='https://github.com/SylwiaOliwia2/xgboost-AutoTune',
+    py_modules=['xgboost_autotune'],
+    install_requires=['sklearn', 'numpy', 'math', 'copy'],
+)
